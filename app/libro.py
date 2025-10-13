6e1ab457706f79eec505ec22ad623b3437a03bd2
Juann Lozano <jdlozano58@ucatolica.edu.co>
2025-10-12 23:55:38 -0500

    holis

---
+++ app/libro.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Libro:
+    id_libro: int
+    titulo: str
+    autor: str
+    categoria: str
+    anio_publicacion: int
+    lista_reviews: List['Review'] = []
+    calificacion_promedio: float = 0.0
+
+    def obtener_descripción(self) -> str:
+        pass
+
+    def obtener_promedio_calificaciones(self, calificaciones: List[float]) -> float:
+        pass
+
+    def agregar_review(self, review: 'Review') -> None:
+        pass
+
+    def mostrar_reviews(self) -> str:
+        pass
