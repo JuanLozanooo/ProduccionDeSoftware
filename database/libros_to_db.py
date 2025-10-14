4174476d677b6eb6415de8ba88c021b4d9a040a0
Juann Lozano <jdlozano58@ucatolica.edu.co>
2025-10-13 19:00:10 -0500

    fiumbaaa

---
+++ database/libros_to_db.py
@@ -0,0 +1,37 @@
+import csv
+from sqlmodel import Field, SQLModel, create_engine, Session
+from typing import Optional
+
+class Libro(SQLModel, table=True):
+    id_libro: Optional[int] = Field(default=None, primary_key=True)
+    titulo: str = Field(..., min_length=1, max_length=200)
+    autor: str = Field(..., min_length=1, max_length=100)
+    categoria: str = Field(..., min_length=1, max_length=100)
+    anio_publicacion: int = Field(..., ge=0, le=2025)
+
+DATABASE_URL = "postgresql://ucqqnav94roh5id44ztb:1QrDned1fmQUTycUL6YZhEY58oJhpn@bgkccaor3afzq4gqzc6c-postgresql.services.clever-cloud.com:50013/bgkccaor3afzq4gqzc6c"
+engine = create_engine(DATABASE_URL)
+
+def create_table():
+    SQLModel.metadata.create_all(engine)
+
+def insert_books_from_csv(csv_path: str):
+    with open(csv_path, newline='', encoding="utf-8") as csvfile:
+        reader = csv.DictReader(csvfile)
+        entries = []
+
+        for row in reader:
+            row["id_libro"] = int(row["id_libro"])
+            row["anio_publicacion"] = int(row["anio_publicacion"])
+
+            libro = Libro(**row)
+            entries.append(libro)
+
+        with Session(engine) as session:
+            session.add_all(entries)
+            session.commit()
+
+if __name__ == "__main__":
+    create_table()
+    insert_books_from_csv("libros.csv")
+    print("📚 Datos de libros.csv insertados correctamente.")
---
+++ database/usuarios_to_db.py
@@ -0,0 +1,38 @@
+import csv
+from sqlmodel import Field, SQLModel, create_engine, Session
+from typing import Optional
+import os
+
+class Usuario(SQLModel, table=True):
+    id_usuario: Optional[int] = Field(default=None, primary_key=True)
+    username: str = Field(..., min_length=3, max_length=50)
+    email_usuario: str = Field(..., min_length=5, max_length=100)
+    password: str = Field(..., min_length=3, max_length=100)
+    activo: bool = Field(default=True)
+
+DATABASE_URL = "postgresql://ucqqnav94roh5id44ztb:1QrDned1fmQUTycUL6YZhEY58oJhpn@bgkccaor3afzq4gqzc6c-postgresql.services.clever-cloud.com:50013/bgkccaor3afzq4gqzc6c"
+engine = create_engine(DATABASE_URL)
+
+def create_table():
+    SQLModel.metadata.create_all(engine)
+
+def insert_users_from_csv(csv_path: str):
+    with open(csv_path, newline='', encoding="utf-8") as csvfile:
+        reader = csv.DictReader(csvfile)
+        entries = []
+
+        for row in reader:
+            row["id_usuario"] = int(row["id_usuario"])
+            row["activo"] = row["activo"].strip().lower() == "true"
+
+            user = Usuario(**row)
+            entries.append(user)
+
+        with Session(engine) as session:
+            session.add_all(entries)
+            session.commit()
+
+if __name__ == "__main__":
+    create_table()
+    insert_users_from_csv("usuarios.csv")
+    print("👤 Datos de usuarios insertados correctamente.")
